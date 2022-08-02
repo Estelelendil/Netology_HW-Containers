9331817f14bae46fd9092ef1c7f37bdc5f097135
Чернова Александра <estelelendil@mail.ru>
2022-08-02 19:47:28 +0300

    code worked and tested, 100 percent coverage

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -9,27 +9,55 @@ const persBowman = new Bowman('Susanna');
 const persUndead = new Undead('Shon');
 const persMagician = new Magician('Din');
 const persZombie = new Zombie('Will');
-const myDreamTeam = new Team();
 
-test('create class', () => {
-  const result = { members: new Set() };
-  expect(result).toEqual(myDreamTeam);
-});
+describe('Team test', () => {
+  let result;
+  let myDreamTeam;
 
-test('added member', () => {
-  const result = {
-    'members': Set {
-      Daemon {
-         attack: 10,
-        defence: 40,
-        health: 100,
-        level: 1,
-        name: 'Larry',
-        type: 'Daemon',
-      },
-    }
-  };
-  // result.add(persDaemon);
-  myDreamTeam.add(persDaemon);
-  expect(result).toEqual(myDreamTeam);
+  beforeEach(() => {
+    result = { members: new Set() };
+    myDreamTeam = new Team();
+  });
+
+  test('create class', () => {
+    expect(result).toEqual(myDreamTeam);
+  });
+
+  test('added member', () => {
+    result.members.add(new Daemon('Larry'));
+    myDreamTeam.add(persDaemon);
+
+    expect(result).toEqual(myDreamTeam);
+  });
+
+  test('added several members', () => {
+    result.members.add(new Daemon('Larry'));
+    result.members.add(new Magician('Din'));
+    result.members.add(new Zombie('Will'));
+    myDreamTeam.addAll(persDaemon, persMagician, persZombie);
+
+    expect(result).toEqual(myDreamTeam);
+  });
+
+  test('testing error added equal pers', () => {
+    myDreamTeam.add(persDaemon);
+    expect(() => myDreamTeam.add(persDaemon)).toThrow();
+  });
+
+  test('testing error added several equal pers', () => {
+    result.members.add(new Daemon('Larry'));
+    result.members.add(new Magician('Din'));
+    result.members.add(new Zombie('Will'));
+    myDreamTeam.addAll(persDaemon, persMagician, persZombie, persDaemon);
+
+    expect(result).toEqual(myDreamTeam);
+  });
+
+  test('testing convert to Array', () => {
+    result.members.add(new Daemon('Larry'));
+    myDreamTeam.add(persDaemon);
+    const newresult = Array.from(result.members);
+
+    expect(newresult).toEqual(myDreamTeam.toArray());
+  });
 });
